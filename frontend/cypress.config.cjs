7bd2a7ef5745dc85b3d5852cf21bc44322f7dc7c
WillAxe <wilaxe1129@gmail.com>
2025-12-02 13:34:13 +0100

    Added some files

--- frontend/cypress.config.cjs
+++ frontend/cypress.config.cjs
@@ -11,6 +11,7 @@ const { defineConfig } = require("cypress")
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "http://localhost:5173",
     async setupNodeEvents(on, config) {
       const bundler = createBundler({
         plugins: [createEsbuildPlugin(config)],
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,27 @@
+import Home from "./components/home"
+import Signin from "./components/sign-in"
+import Login from "./components/login"
+import LandingPage from "./components/landing-page"
+import { createHashRouter, Outlet, RouterProvider } from "react-router-dom"
+
 function App() {
-  return <></>
+  const router = createHashRouter([
+    {
+      children: [
+        { element: <Home />, path: "/" },
+        { element: <Signin />, path: "/signin" },
+        { element: <Login />, path: "/login" },
+        { element: <LandingPage />, path: "/landingpage/:userid" },
+      ],
+      element: (
+        <>
+          <Outlet />
+        </>
+      ),
+    },
+  ])
+
+  return <RouterProvider router={router}></RouterProvider>
 }
 
 export default App
