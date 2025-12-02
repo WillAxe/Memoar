56383cceed65576aca59a4daea644e9c61bd406b
WillAxe <wilaxe1129@gmail.com>
2025-12-02 15:43:59 +0100

    Fixed a samll bug in the test

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -7,7 +7,7 @@ describe("the page and make sure that it loads and test the basic functionlaity 
   })
 
   it("has a navbar for user to navigate with", () => {
-    cy.get("[data-cy='navigation-bar]'").should("exist")
+    cy.get("[data-cy='navigation-bar']").should("exist")
     cy.get("[data-cy='navigation-bar']").within(() => {
       cy.get("[data-cy='signin-link']").click()
       cy.url().should("include", "/signup")
