d7355275a48f44fc7ed93577aca226d3383638bb
WillAxe <wilaxe1129@gmail.com>
2025-12-03 13:48:16 +0100

    Added the navbar functionality

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,4 +1,19 @@
+import { Link } from "react-router-dom"
 function Navbar() {
-  return <></>
+  return (
+    <>
+      <nav data-cy="navigation-bar">
+        <Link data-cy="home-navigation-link" to="/">
+          Home
+        </Link>
+        <Link data-cy="signup-link" to="/signup">
+          Sign up
+        </Link>
+        <Link data-cy="login-link" to="/login">
+          Login
+        </Link>
+      </nav>
+    </>
+  )
 }
 export default Navbar
