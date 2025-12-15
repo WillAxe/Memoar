6a08e4a8ed97ddf464475cd6fb0b5e8601a81466
WillAxe <wilaxe1129@gmail.com>
2025-12-15 09:37:05 +0100

    changed yaml and dockerfile

--- backend/index.ts
+++ backend/index.ts
@@ -21,15 +21,15 @@ app.use("/api", userRoutes)
 app.use("/api", roomRoutes)
 app.use("/api", postRoutes)
 app.use("/api", feedRoutes)
+// app.use(express.static("public"))
 
-// app.use(express.static(path.join(path.resolve(), "dist")))
-app.use(express.static("public"))
-
-app.get("*", (_req, res) => {
-  res.sendFile(path.resolve("public/index.html"))
-})
+// app.get("*", (_req, res) => {
+//   res.sendFile(path.resolve("public/index.html"))
+// })
 
 app.use("/uploads", express.static("uploads"))
+
+app.use(express.static(path.join(path.resolve(), "dist")))
 
 app.listen(3000, () => {
   console.log(`Redo på http://localhost:${port}`)
